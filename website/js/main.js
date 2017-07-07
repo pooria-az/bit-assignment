53be212421608aef1e950b6a4bdc5d6c2838db57
pooria-az <cyrus.mark@gmail.com>
2017-07-07 15:36:51 +0800

    Update main.js

--- website/js/main.js
+++ website/js/main.js
@@ -1,18 +1,21 @@
-var yahooRSS = 'https://finance.yahoo.com/rss/topstories';
+var endpoint = 'https://newsapi.org/v1/articles?source=the-economist&sortBy=latest&apiKey=dc4ac6237d1c4033953786fd575b6254';
 
 $('body').ready(function() {
-  $.get(yahooRSS, function(data) {
-    var $xml = $(data);
-    $xml.find("item").each(function() {
-      var $this = $(this);
-      var item = {
-        title: $this.find("title").text(),
-        link: $this.find("link").text(),
-        description: $this.find("description").text(),
-        pubDate: $this.find("pubDate").text(),
-        author: $this.find("author").text()
-      };
-      console.log(item);
+  $.get(endpoint, function(data) {
+    if (data.status !== 'ok' || !data.articles) {
+      return;
+    }
+    data.articles.slice(0, 2).forEach(function(article) {
+      $('#news1-row').append(`<a class="news-title" target="_blank" href="${article.url}" >${article.title}</a><p class="news-text">${article.description || ''}</p>`);
     });
+
+    data.articles.slice(2, 4).forEach(function(article) {
+      $('#news2-row').append(`<a class="news-title" target="_blank" href="${article.url}" >${article.title}</a><p class="news-text">${article.description || ''}</p>`);
+    })
+
+    data.articles.slice(4, 6).forEach(function(article) {
+      $('#news3-row').append(`<a class="news-title" target="_blank" href="${article.url}" >${article.title}</a><p class="news-text">${article.description || ''}</p>`);
+    })
+    
   });
 });
